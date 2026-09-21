319444cf212424b82afc1fccf9815cfbf1aaf8c9
Asca_ <abdoulascandrou@gmail.com>
2026-09-21 16:50:55 +0000

    script.js

--- script.js
+++ script.js
@@ -3,23 +3,27 @@ const downloadCount = document.getElementById("downloadCount");
 async function afficherTelechargements() {
     try {
         const response = await fetch(
-            "https://api.github.com/repos/4sc4/IPO/releases/latest"
+            "https://api.github.com/repos/4sc4/IPO/releases"
         );
 
         if (!response.ok) {
-            throw new Error("Aucune release trouvée");
+            throw new Error("Impossible de récupérer les Releases");
         }
 
-        const release = await response.json();
+        const releases = await response.json();
 
-        const apk = release.assets.find(
-            asset => asset.name.toLowerCase() === "ipo.apk"
-        );
+        let total = 0;
 
-        if (apk) {
-            downloadCount.textContent =
-                apk.download_count.toLocaleString("fr-FR");
-        }
+        releases.forEach(release => {
+            release.assets.forEach(asset => {
+                if (asset.name.toLowerCase() === "ipo.apk") {
+                    total += asset.download_count;
+                }
+            });
+        });
+
+        downloadCount.textContent = total.toLocaleString("fr-FR");
+
     } catch (error) {
         console.log("Compteur :", error.message);
     }
